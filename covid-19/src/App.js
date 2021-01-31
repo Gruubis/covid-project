e2a88c9d522ae150b49570cca8352201872a6289
Domantas Grubis <s037814@ad.viko.lt>
2021-01-31 23:53:01 +0200

    almost done

--- covid-19/src/App.js
+++ covid-19/src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
-import { BasicTable } from "./components/BasicTable";
-import Table from "./components/Table";
+import Particles from "react-particles-js";
+import "./App.css";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -11,69 +23,82 @@ class App extends React.Component {
     };
     this.handleSelect = this.handleSelect.bind(this);
   }
-
-  async componentDidMount() {
-    const url = "http://localhost:3000/getCountries";
-    const response = await fetch(url);
-    const data = await response.json();
-    this.setState({ countries: data });
-  }
-  handleSelect = (event) => {
-  let url = new URL("http://localhost:3000/"+ event.target.value);
- fetch(url)
- .then(response => response.json())
- .then(response => {
-   this.setState({ data: response})
-  console.log(response)
-  })
-  
+componentDidMount() {
+  fetch("http://localhost:3000/getCountries")
+  .then((response) => response.json())
+  .then((response) => {
+    this.setState({ countries: response})
+  })}
+handleSelect = (event) => {
+    let url = new URL("http://localhost:3000/" + event.target.value);
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ data: response });
+      });
   };
   render() {
     return (
-      <div className="tc">
-        <h1> Pick a country </h1>
-        <label>
-          <select
-            onChange={this.handleSelect}
-            className="pa2 ba b--green bg=lighest-blue"
-          >
-            {this.state.countries.map((country) => (
-              <option key={country} value={country}>
-                {country}{" "}
-              </option>
-            ))}
-          </select>
-        </label>
-        <h2>{this.state.data.country}</h2>
-        <table>
-          <thead className="tc">
-            <tr>
-              <th>Year/week</th>
-              <th>Deaths</th>
-              <th>Cases</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.data.length > 0 ? (
-              this.state.data.map((dat, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{dat.week}</td>
-                    <td>{dat.deaths}</td>
-                    <td>{dat.cases}</td>
+      <>
+        <Particles className="particles" params={particlesOptions} />
+        <div className="tc content">
+          <h1> Pick a country </h1>
+          <label>
+            <select
+              onChange={this.handleSelect}
+              className="pa2 ba b--green bg=lighest-blue"
+            >
+              {this.state.countries.map((country) => (
+                <option key={country} value={country}>
+                  {country}{" "}
+                </option>
+              ))}
+            </select>
+          </label>
+          <div className="pa4">
+            <div className="overflow-auto">
+              <table className="fw4 w-100 mw6 center" cellSpacing="0">
+                <thead>
+                  <tr>
+                    <th className="fw8 tc bb b--black-20 tl pb3 pr3 0-50 ">
+                      Year-Week
+                    </th>
+                    <th className="fw8 tc bb b--black-20 tl pb3 pr3 ">
+                      Deaths
+                    </th>
+                    <th className="fw8 tc bb b--black-20 tl pb3 pr3 ">
+                      Cases</th>
                   </tr>
-                );
-              })
-            ) : (
-              <tr>
-                <td> LOADING...</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
+                </thead>
+                <tbody className="lh-copy">
+                  {this.state.data.length > 0 ? (
+                    this.state.data.map((dat, index) => {
+                      return (
+                        <tr key={index}>
+                          <td className="tc pv1 pr3 bb b--black-20">
+                            {dat.week}
+                          </td>
+                          <td className="tc pv1 pr3 bb b--black-20">
+                            {dat.deaths}
+                          </td>
+                          <td className="tc pv1 pr3 bb b--black-20">
+                            {dat.cases}
+                          </td>
+                        </tr>
+                      );
+                    })
+                  ) : (
+                    <tr>
+                      <td> </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
-
 export default App;
--- covid_server/server.js
+++ covid_server/server.js
@@ -1,34 +1,33 @@
 const express = require("express");
 const request = require("request");
-const { Countries, Data } = require("./countries.js");
+const { Countries, Data } = require("./functions.js");
 const cors = require("cors");
 
 const app = express();
-// 
 
 app.use(cors());
 let countries;
-let datas;
+let data;
 
-Data((op) => {
-    datas = op;
-})
+Data((info) => {
+  data = info;
+});
 Countries((country) => {
   countries = country;
 });
 
-app.get('/getCountries', (req, res) => {
-  res.send(countries) 
-})
+app.get("/getCountries", (req, res) => {
+  res.send(countries);
+});
 
 app.get("*", (req, res) => {
-  let country = req.params
-  const countryString = JSON.stringify(country)
-  country = countryString.slice(7, countryString.length - 2)
-  const info = datas.filter((data) => data.country === country);
+  let country = req.params;
+  const countryString = JSON.stringify(country);
+  country = countryString.slice(7, countryString.length - 2);
+  const info = data.filter((data) => data.country === country);
   res.send(info);
 });
 
 app.listen(3000, () => {
-  console.log("test");
+  console.log("Listening on 3000");
 });
